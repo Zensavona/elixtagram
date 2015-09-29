dbca22ba65298f96d29bc77ecfda005f9d8164d4
Zen Savona <z@zensavona.com>
2015-09-29 02:12:43 +0200

    clean up a bit

--- lib/elixtagram/api/base.ex
+++ lib/elixtagram/api/base.ex
@@ -8,17 +8,11 @@ defmodule Elixtagram.API.Base do
     "#{@base_url}#{meat}?client_id=#{client_id}"
   end
 
-  def process_response_body(body) do
-    # res = Poison.decode! body, as: Elixtagram.Model.Response
-    # res.data
-    body
-      |> Poison.decode!
-      |> Dict.take(["data"])
-      |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  def get(url_part) do
+    data = get!(url_part).body
   end
 
-  def request(:get, url_part) do
-    get!(url_part).body[:data]
-      |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  def extract_data(struct) do
+    struct["data"]
   end
 end
--- lib/elixtagram/api/tags.ex
+++ lib/elixtagram/api/tags.ex
@@ -2,7 +2,8 @@ defmodule Elixtagram.API.Tags do
   import Elixtagram.API.Base
 
   def tag(tag_name) do
-    tag = Elixtagram.API.Base.request(:get, "/tags/#{tag_name}")
-    %Elixtagram.Model.Tag{name: tag[:name], media_count: tag[:media_count]}
+    structure = Elixtagram.API.Base.get("/tags/#{tag_name}")
+      |> Poison.decode! as: %{"data" => Elixtagram.Model.Tag}
+    structure["data"]
   end
 end

8473c26351163df7ab48c94bb86edc4726f39be3
Zen Savona <z@zensavona.com>
2015-09-29 16:45:09 +0200

    inch, coveralls

--- mix.exs
+++ mix.exs
@@ -7,31 +7,36 @@ defmodule Elixtagram.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
+     test_coverage: [tool: ExCoveralls],
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :httpoison, :oauth2]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.7.4"},
       {:poison, "~> 1.5"},
       {:oauth2, "~> 0.3"},
-      {:ex_doc, github: "elixir-lang/ex_doc"}
+      {:ex_doc, github: "elixir-lang/ex_doc", only: :docs},
+      {:excoveralls, "~> 0.3", only: [:dev, :test]},
+      {:inch_ex, "~> 0.4.0", only: [:dev, :docs]}
     ]
+  end
+
+  defp description do
+    """
+    Instagram client library for Elixir.
+    """
+  end
+
+  defp package do
+    [ contributors: ["zensavona"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/zensavona/elixtagram"} ]
   end
 end

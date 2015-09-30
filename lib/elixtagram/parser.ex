eed818553d80defca335e2da5795ab847c36d55f
Zen Savona <z@zensavona.com>
2015-09-30 19:42:29 +0200

    params parser changes

--- lib/elixtagram/parser.ex
+++ lib/elixtagram/parser.ex
@@ -7,4 +7,20 @@ defmodule Elixtagram.Parser do
   def parse_media(object) do
     struct(Elixtagram.Model.Media, object)
   end
+
+  def parse_location(object) do
+    struct(Elixtagram.Model.Location, object)
+  end
+
+  @doc """
+  Parse request parameters for the API.
+  """
+  def parse_request_params(options) do
+    Enum.map(options, fn({k,v}) -> [to_string(k), to_string(v)] end)
+  end
+  def parse_request_params(options, accepted) do
+    Enum.filter_map(options,
+      fn({k,v}) -> Enum.member?(accepted, k) end,
+      fn({k,v}) -> [to_string(k), to_string(v)] end)
+  end
 end

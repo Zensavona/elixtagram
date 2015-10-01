b5bb4e1f1f3c1973e83e07c8921d49861aba0484
Zen Savona <z@zensavona.com>
2015-10-01 10:56:17 +0200

    package.exs for hex.pm later

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "elixtagram", description: "Instagram client library for elixir",
+                 version: "1.0.0", keywords: ["Elixir","Instagram","REST"],
+                 maintainers: [[name: "zensavona", email: "z@zen.id.au"]],
+                 repositories: [[github: "zensavona/elixtagram"]])

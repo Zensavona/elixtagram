5a58adb3110058f498cf2c1d24b3470b7673607c
Zen Savona <z@zensavona.com>
2015-10-01 19:44:26 +0200

    don't need this, it's auto generated

--- package.exs
+++
@@ -1,4 +0,0 @@
-Expm.Package.new(name: "elixtagram", description: "Instagram client library for elixir",
-                 version: "1.0.0", keywords: ["Elixir","Instagram","REST"],
-                 maintainers: [[name: "zensavona", email: "z@zen.id.au"]],
-                 repositories: [[github: "zensavona/elixtagram"]])
